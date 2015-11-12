5d24f8218f021c45e3c892cb7680a9fd225e35ea
mk27@hawaii.edu <myoung@myounghoon-ui-MacBook-Air.local>
2015-11-12 10:53:31 -1000

    class

--- week-12/exercise/script.js
+++ week-12/exercise/script.js
@@ -1,15 +1,38 @@
-var myName
-var student_1
-var student_2
-var student_3
-var student_4
-var student_5
-var student_6
-var student_7
-var student_8
-var student_9
-var student_10 
+var myName = 'Myoung';
+var student_1 = 'Sam';
+var student_2 = 'Sasha'
+var student_3 = 'Hyela'
+var student_4 = 'Volha'
+var student_5 = 'Myoung';
+var student_6 = 'Mayu'
+var student_7 = 'Yosh'
+var student_8 = 'Mark'
+var student_9 = 'Jae'
+var student_10 = 'Kalena'
 
-var intro = "Hello world, my name is"
+var intro = "Hello world, my name is ";
 
-var studentStatus = "is a current student"
+var studentStatus = " is a current student";
+
+console.log(intro + myName);
+
+console.log(myName + studentStatus);
+
+if (myName === 'Sam') {
+  console.log(intro + myName);
+} else {
+  console.log('Your name is not really Sam');
+}
+
+
+
+// console.log(student_1 + studentStatus);
+// console.log(student_2 + studentStatus);
+// console.log(student_3 + studentStatus);
+// console.log(student_4 + studentStatus);
+// console.log(student_5 + studentStatus);
+// console.log(student_6 + studentStatus);
+// console.log(student_7 + studentStatus);
+// console.log(student_8 + studentStatus);
+// console.log(student_9 + studentStatus);
+// console.log(student_10 + studentStatus);
