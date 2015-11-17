a5beccaa2f52869297b053893862975733b1a763
mk27@hawaii.edu <myoung@myounghoon-ui-MacBook-Air.local>
2015-11-17 10:55:12 -1000

    v2

--- week-12/exercise/script.v2.js
+++ week-12/exercise/script.v2.js
@@ -1,9 +1,17 @@
 // Create an array including the first names of each student in the class
-var classMates;
+var classMates = ['Sam', 'Sasha', 'Hyela', 'Volha', 'Myoung', 'Mayu', 'Yosh', 'Mark', 'Jae', 'Kalena'];
 
-// Student status string
+var student = {
+  
+}
+
+
+}// Student status string
 var studentStatus = ' is an interface design student.';
 
 // Display in the console the status of each student,
-// using a for loop and one console statement. 
-console.log(classMates + studentStatus);
+// using a for loop and one console statement.
+
+for (var i = 0; i < classmates.length;i++){
+  console.log(classMates + studentStatus);
+}
